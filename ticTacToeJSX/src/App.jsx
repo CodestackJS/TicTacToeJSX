690151bbf6fbb91395810bd0a5b7ea98ccfe0ee9
Jerie See Villanueva <codestackjs007@gmail.com>
2024-06-18 20:50:46 -0700

    TicTacToe initial commit

---
+++ ticTacToeJSX/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Square from './Components/Square'
+import Board from './Components/Board'
+
+const App = () => {
+  return (
+    <>
+    {/* <Square/> */}
+    <Board/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ ticTacToeJSX/src/Components/Board.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import Square from './Square'
+
+
+
+
+const Board = () => {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
+    const handleClick = (i) => {
+        const nextSquares = squares.slice();
+        nextSquares[i] = "X";
+        setSquares(nextSquares);
+    }
+
+
+  return (
+    <>
+    
+    <div className="board-row">
+    <Square value ={squares[0]} onSqareclick={() => handleClick(0)}/> 
+    <Square value ={squares[1]} onSqareclick={() => handleClick(1)}/> 
+    <Square value ={squares[2]} onSqareclick={() => handleClick(2)}/> 
+    
+    
+    </div>
+    <div className="board-row">
+    <Square value ={squares[3]} onSqareclick={() => handleClick(3)}/> 
+    <Square value ={squares[4]} onSqareclick={() => handleClick(4)}/> 
+    <Square value ={squares[5]} onSqareclick={() => handleClick(5)}/> 
+    </div>
+
+
+    <div className="board-row">
+    <Square value ={squares[6]} onSqareclick={() => handleClick(6)}/> 
+    <Square value ={squares[7]} onSqareclick={() => handleClick(7)}/> 
+    <Square value ={squares[8]} onSqareclick={() => handleClick(8)}/> 
+
+    </div>
+
+    
+    </>
+  )
+}
+
+export default Board
